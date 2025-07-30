c1efd42737aec4260e765284fd7f047d86a3535c
Lu <l.han991230@gmail.com>
2025-07-30 15:45:44 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Lu Han on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
